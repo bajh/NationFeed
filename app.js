fed8e24c5624e84d3b19e0c49eff1deac0e4434c
Brandon Hopkins <brhpkns@gmail.com>
2014-09-08 22:14:44 -0400

    AJAX requests on map clicks

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
 var app = require('express')(),
+    url = require('url'),
+    queryString = require('querystring'),
     express = require('express'),
     port = process.env.PORT || 3000,
-    http = require('http').Server(app),
-    io = require('socket.io')(http);
+    http = require('http').Server(app);
 
 app.use(express.static(__dirname + '/public'));
+
+app.get('/tweets-for-state', function(req, res){
+  reqUrl = url.parse(req.url);
+  // queryOb = queryString.parse(reqUrl.query);
+  // jsonReq = JSON.parse(queryOb);
+  state = reqUrl.query;
+  res.send({"msg": "You submitted a request for " + state});
+});
 
 http.listen(port, function(){});
---
+++ public/map_behavior.js
@@ -0,0 +1,13 @@
+$(function(){
+  $('#us_map').on('click', 'path', function(){
+    clickedState = $(this).attr('id');
+    $.ajax({
+      url: '/tweets-for-state',
+      data: clickedState,
+      dataType: 'json',
+      success: function(res){
+        console.log(res.msg);
+      }
+    })
+  })
+})
